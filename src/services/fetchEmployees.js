452a25a0ba29763aeedf3554f1298569033d1b5b
f3ktr <46202047+f3ktr@users.noreply.github.com>
2021-08-10 23:44:30 +0200

    Update fetchEmployees.js

--- src/services/fetchEmployees.js
+++ src/services/fetchEmployees.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const fetchEmployees = async (name = "", office = "", sort = "ASC") => {
   let local_URL = "http://localhost:5000";
-  let remote_URL = "https://ninja-scrapper.herokuapp.com";
+  let remote_URL = "https://ninja-scrapper-tretton37.herokuapp.com";
   let users;
   await axios
     .get(`${local_URL}/employees`, {
