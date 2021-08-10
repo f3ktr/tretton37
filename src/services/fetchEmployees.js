bca365eae4686914b773ed0f4c3dcef890bc069d
f3ktr <46202047+f3ktr@users.noreply.github.com>
2021-08-10 23:45:06 +0200

    Update fetchEmployees.js

--- src/services/fetchEmployees.js
+++ src/services/fetchEmployees.js
@@ -5,12 +5,7 @@ const fetchEmployees = async (name = "", office = "", sort = "ASC") => {
   let remote_URL = "https://ninja-scrapper-tretton37.herokuapp.com";
   let users;
   await axios
-    .get(`${local_URL}/employees`, {
-      headers: {
-        Authorization:
-          "api-key 14:2021-07-13:petra.lichtenecker@tretton37.com eb67f6c323efe30b873000fedfb80c2592ae69c5bb88552884919a762efafa18",
-        "Access-Control-Allow-Origin": "*",
-      },
+    .get(`${remote_URL}/employees`, {
       params: {
         name: name,
         office: office,
